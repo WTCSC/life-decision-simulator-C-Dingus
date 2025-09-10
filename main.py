1e26788edadaa5fec6efd602833cc6ea8a01e839
C-Dingus <rickardc@CSGP.EDU>
2025-09-10 08:48:26 -0600

    wait there is good code?

--- main.py
+++ main.py
@@ -1,106 +1,38 @@
+#import string patterns
+import re
+
+#Reads the one message in the message in the file and gets the message, where choice one goes, where choice 2 goes
 def read_msg(message):
+    #Open the file
     with open("messages.ook", "r") as file:
-        return file.read().split(f"%msg{message}%")[1].strip()
-
-choice = input(f"{read_msg(0)}\n")
-
-if choice == "1":
-    choice = input(f"{read_msg(1)}\n")
-
-    choice = input(f"{read_msg(3)}\n")
-    if choice == "1":
-        choice = input(f"{read_msg(6)}\n")
-        choice = input(f"{read_msg(7)}\n")
-        choice = input(f"{read_msg(10)}\n")
-        choice = input(f"{read_msg(14)}\n")
-        choice = input(f"{read_msg(16)}\n")
-        #END
-    else:
-        choice = input(f"{read_msg(7)}\n")
-        choice = input(f"{read_msg(10)}\n")
-        choice = input(f"{read_msg(14)}\n")
-        choice = input(f"{read_msg(16)}\n")
-        #END
-
-else:
-    choice = input(f"{read_msg(2)}\n")
-    if choice == "1":
-        while True:
-            choice = input(f"{read_msg(4)}\n")
-            if choice != "1":
-                choice = input(f"{read_msg(5)}\n")
-                if choice == "1":
-                    choice = input(f"{read_msg(9)}\n")
-                    if choice == "1":
-                        choice = input(f"{read_msg(12)}\n")
-                        if choice == "1":
-                            choice = input(f"{read_msg(13)}\n")
-                            if choice == "1":
-                                choice = input(f"{read_msg(15)}\n")
-                                choice = input(f"{read_msg(14)}\n")
-                                choice = input(f"{read_msg(16)}\n")
-
-                                break
-                    else:
-                        choice = input(f"{read_msg(13)}\n")
-                        if choice == "1":
-                            choice = input(f"{read_msg(15)}\n")
-                            choice = input(f"{read_msg(14)}\n")
-                            choice = input(f"{read_msg(16)}\n")
-                            break
+        #Get the correct message
+        message = file.read().split(f"%msg{message}%")[1].strip()
+        #If it has any options
+        try:
+            options = message.split("1)")[1].split("\n")
+            #If the player only has 1 option
+            if len(options) > 1:
+                return [message, options[0].split("%")[1], options[1].split("%")[1]]
             else:
-                choice = input(f"{read_msg(8)}\n")
-                if choice == "1":
-                    choice = input(f"{read_msg(5)}\n")
-                    if choice == "1":
-                        choice = input(f"{read_msg(9)}\n")
-                        if choice == "1":
-                            choice = input(f"{read_msg(12)}\n")
-                            if choice == "1":
-                                choice = input(f"{read_msg(13)}\n")
-                                if choice == "1":
-                                    choice = input(f"{read_msg(15)}\n")
-                                    choice = input(f"{read_msg(14)}\n")
-                                    choice = input(f"{read_msg(16)}\n")
-                                    break
-                        else:
-                            choice = input(f"{read_msg(13)}\n")
-                            if choice == "1":
-                                choice = input(f"{read_msg(15)}\n")
-                                choice = input(f"{read_msg(14)}\n")
-                                choice = input(f"{read_msg(16)}\n")
-                                break
+                return [message, options[0].split("%")[1], options[0].split("%")[1]]
+        except:
+            #end the sim
+            return [message, "break", "break"]
+
+#Set what message to read
+choice = 0
+while True:
+    #Read the message and get the option destinations
+    buf = read_msg(choice)
+    
+    #Get user input of visual message
+    i = input(f"{re.sub(r'%..%', '', re.sub(r'%.%', '', buf[0]))}\n")
+
+    #Repeat with new message or end sim
+    if buf[1] == "break":
+        break
+    if i == "1":
+        choice = buf[1]
     else:
-        while True:
-            choice = input(f"{read_msg(5)}\n")
-            if choice == "1"
-                if choice == "1":
-                    choice = input(f"{read_msg(9)}\n")
-                    if choice == "1":
-                        choice = input(f"{read_msg(12)}\n")
-                        if choice == "1":
-                            choice = input(f"{read_msg(13)}\n")
-                            if choice == "1":
-                                choice = input(f"{read_msg(15)}\n")
-                                choice = input(f"{read_msg(14)}\n")
-                                choice = input(f"{read_msg(16)}\n")
-                                break
-                        else:
-                            choice = input(f"{read_msg(13)}\n")
-                            if choice == "1":
-                                choice = input(f"{read_msg(15)}\n")
-                                choice = input(f"{read_msg(14)}\n")
-                                choice = input(f"{read_msg(16)}\n")
-                                break
-            else:
-                choice = input(f"{read_msg(4)}\n")
-                if choice != "1":
-                    choice = input(f"{read_msg(8)}\n")
-                    if choice == "1":
-
-
-
-
-
-
+        choice = buf[2]
 
